21345b7ceb6ac6a80a8adfcfb84daaccaf8bd9e1
黄可 <huangke@jd.com>
2018-08-24 11:07:59 +0800

    [Mas]mas_distributeViewsAlongAxis

--- iOSSizeClasses/iOSSizeClasses/MasBasicViewController.m
+++ iOSSizeClasses/iOSSizeClasses/MasBasicViewController.m
@@ -16,16 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIView *superView = self.view;
-    UIView *view1 = UIView.new;
-    view1.backgroundColor = [UIColor greenColor];
-    [superView addSubview:view1];
+    self.view.backgroundColor = UIColor.whiteColor;
     
-    UIEdgeInsets padding = UIEdgeInsetsMake(10, 10, 10, 10);
-    [view1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(superView).with.insets(padding);
+    UIView *baseView = [[UIView alloc]initWithFrame:CGRectMake(0, 100, 200, 300)];
+    baseView.backgroundColor = [UIColor brownColor];
+    [self.view addSubview:baseView];
+    
+    NSMutableArray *array = [NSMutableArray new];
+    for (int i = 0; i < 5; i ++) {
+        UIView *view = [UIView new];
+        view.backgroundColor = [UIColor greenColor];
+        [baseView addSubview:view];
+        [array addObject:view]; //保存添加的控件
+    }
+    
+    //水平方向控件间隔固定等间隔
+    [array mas_distributeViewsAlongAxis:MASAxisTypeVertical withFixedItemLength:30 leadSpacing:10 tailSpacing:10];
+    [array makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(30);
+        make.width.equalTo(@80);
     }];
     
+//    //水平方向宽度固定等间隔
+//    [array mas_distributeViewsAlongAxis:MASAxisTypeHorizontal withFixedItemLength:70 leadSpacing:10 tailSpacing:10];
+//    [array makeConstraints:^(MASConstraintMaker *make) { //数组额你不必须都是view
+//        make.top.equalTo(@50);
+//        make.height.equalTo(@70);
+//    }];
+
 }
 
 - (void)didReceiveMemoryWarning {
