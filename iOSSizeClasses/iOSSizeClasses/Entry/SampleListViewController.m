b619812db7f5abd3d2e552e3b6d771001b7345a0
黄可 <huangke@jd.com>
2018-10-17 11:19:04 +0800

    commit

--- iOSSizeClasses/iOSSizeClasses/Entry/SampleListViewController.m
+++ iOSSizeClasses/iOSSizeClasses/Entry/SampleListViewController.m
@@ -39,41 +39,7 @@
                          [[ViewController alloc] initWithTitle:@"SizeClasses" andIdentifierStr:@"ViewController"],
                          [[MasUseListViewController alloc] initWithTitle:@"masonry使用列表"]
                          ];
-    [self dispatchSignal];
-}
-
-
-- (void)dispatchSignal {
-    //crate的value表示，最多几个资源可访问
-    dispatch_semaphore_t semaphore = dispatch_semaphore_create(1);
-    dispatch_queue_t quene = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     
-    //任务1
-    dispatch_async(quene, ^{
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-        NSLog(@"run task 1");
-        sleep(1);
-        NSLog(@"complete task 1");
-        dispatch_semaphore_signal(semaphore);
-    });
-    
-    //任务2
-    dispatch_async(quene, ^{
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-        NSLog(@"run task 2");
-        sleep(1);
-        NSLog(@"complete task 2");
-        dispatch_semaphore_signal(semaphore);
-    });
-    
-    //任务3
-    dispatch_async(quene, ^{
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-        NSLog(@"run task 3");
-        sleep(1);
-        NSLog(@"complete task 3");
-        dispatch_semaphore_signal(semaphore);
-    });
 }
 
 - (void)setupFloatView {
