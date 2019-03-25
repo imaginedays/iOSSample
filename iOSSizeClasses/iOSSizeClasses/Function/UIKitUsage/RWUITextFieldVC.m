f123a1fae4bb25fa73ea76b86d01f1d557efb0ad
黄可 <huangke@jd.com>
2019-03-25 10:01:15 +0800

    UITextField过滤表情字符串

--- iOSSizeClasses/iOSSizeClasses/Function/UIKitUsage/RWUITextFieldVC.m
+++ iOSSizeClasses/iOSSizeClasses/Function/UIKitUsage/RWUITextFieldVC.m
@@ -8,8 +8,8 @@
 
 #import "RWUITextFieldVC.h"
 
-@interface RWUITextFieldVC ()
-
+@interface RWUITextFieldVC ()<UITextFieldDelegate>
+@property (nonatomic, strong) UITextField *inputTextField;
 @end
 
 @implementation RWUITextFieldVC
@@ -17,16 +17,147 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self rw_setupViews];
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Layout
+- (void)rw_setupViews {
+    [self.view addSubview:self.inputTextField];
+    [self rw_makeConstraints];
 }
-*/
 
+- (void)rw_makeConstraints {
+    [self.inputTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.view).offset(200);
+        make.left.mas_equalTo(self.view).offset(20);
+        make.right.mas_equalTo(self.view).offset(-20);
+//        make.height.equalTo(@21);
+    }];
+}
+
+#pragma mark - Private Method
+- (void)rw_textFieldDidChange:(UITextField *)textField {
+    
+}
+
+// 过滤所有表情
+- (BOOL)stringContainsEmoji:(NSString *)string {
+    __block BOOL returnValue = NO;
+    [string enumerateSubstringsInRange:NSMakeRange(0, [string length]) options:NSStringEnumerationByComposedCharacterSequences usingBlock:
+     ^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+         
+         const unichar hs = [substring characterAtIndex:0];
+         // surrogate pair
+         if (0xd800 <= hs && hs <= 0xdbff) {
+             if (substring.length > 1) {
+                 const unichar ls = [substring characterAtIndex:1];
+                 const int uc = ((hs - 0xd800) * 0x400) + (ls - 0xdc00) + 0x10000;
+                 if (0x1d000 <= uc && uc <= 0x1f77f) {
+                     returnValue = YES;
+                 }
+             }
+         } else if (substring.length > 1) {
+             const unichar ls = [substring characterAtIndex:1];
+             if (ls == 0x20e3) {
+                 returnValue = YES;
+             }
+             
+         } else {
+             // non surrogate
+             if (0x2100 <= hs && hs <= 0x27ff) {
+                 returnValue = YES;
+             } else if (0x2B05 <= hs && hs <= 0x2b07) {
+                 returnValue = YES;
+             } else if (0x2934 <= hs && hs <= 0x2935) {
+                 returnValue = YES;
+             } else if (0x3297 <= hs && hs <= 0x3299) {
+                 returnValue = YES;
+             } else if (hs == 0xa9 || hs == 0xae || hs == 0x303d || hs == 0x3030 || hs == 0x2b55 || hs == 0x2b1c || hs == 0x2b1b || hs == 0x2b50) {
+                 returnValue = YES;
+             }
+         }
+     }];
+    
+    return returnValue;
+}
+
+#pragma mark - UITextFieldDelegate
+// return NO to disallow editing.
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    return YES;
+}
+
+// became first responder
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    
+}
+
+// return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    return YES;
+}
+
+// may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    
+}
+
+// if implemented, called in place of textFieldDidEndEditing:  NS_AVAILABLE_IOS(10_0);
+- (void)textFieldDidEndEditing:(UITextField *)textField reason:(UITextFieldDidEndEditingReason)reason {
+    
+}
+
+// return NO to not change text
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    // 不让输入表情
+//    if ([textField isFirstResponder]) {
+//        if ([[[textField textInputMode] primaryLanguage] isEqualToString:@"emoji"] || ![[textField textInputMode] primaryLanguage]) {
+//            NSLog(@"输入的是表情，返回NO");
+//            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"警告！" message:@"不能输入表情" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定",nil];
+//            [alertView show];
+//            return NO;
+//        }
+//    }
+    NSLog(@"textField.text = %@",textField.text);
+    NSLog(@"string = %@",string);
+    
+    if ([self stringContainsEmoji:string]) {
+        NSLog(@"包含表情符号");
+        
+        return NO;
+    } else {
+        NSLog(@"不包含表情符号");
+    }
+    
+    return YES;
+}
+
+// called when clear button pressed. return NO to ignore (no notifications)
+- (BOOL)textFieldShouldClear:(UITextField *)textField {
+    return YES;
+}
+
+// called when 'return' key pressed. return NO to ignore.
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    return YES;
+}
+
+#pragma mark - Accessor
+- (UITextField *)inputTextField {
+    if (!_inputTextField) {
+        _inputTextField = [[UITextField alloc] init];
+        _inputTextField.font = [UIFont systemFontOfSize:14.0f];
+        _inputTextField.placeholder = @"输入内容";
+        _inputTextField.textColor = HexRGB(0x333333);
+        _inputTextField.textAlignment = NSTextAlignmentRight;
+        _inputTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+        _inputTextField.spellCheckingType = UITextSpellCheckingTypeNo;
+        _inputTextField.returnKeyType = UIReturnKeyDone;
+        _inputTextField.delegate = self;
+        _inputTextField.backgroundColor = [UIColor lightGrayColor];
+        [_inputTextField addTarget:self action:@selector(rw_textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    }
+    return _inputTextField;
+}
 @end
--- iOSSizeClasses/iOSSizeClasses/RWCommonMacro.h
+++ iOSSizeClasses/iOSSizeClasses/RWCommonMacro.h
@@ -9,6 +9,11 @@
 #ifndef RWCommonMacro_h
 #define RWCommonMacro_h
 
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "Masonry.h"
+
+#pragma mark - Screen
 #define RW_STATUS_BAR_HEIGHT ([UIDevice isPhoneX] ? 44.0f : 20.0f) //!< 状态栏高度
 #define RW_NAVI_BAR_HEIGHT 44.0f                                   //!< 导航条高度
 
@@ -17,5 +22,30 @@
 #define RW_SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 #define RW_SCREEN_SCALE  ([UIScreen mainScreen].scale)
 
-#import "Masonry.h"
+#define RW_STATUS_BAR_HEIGHT ([UIDevice isPhoneX] ? 44.0f : 20.0f) //!< 状态栏高度
+#define RW_NAVI_BAR_HEIGHT 44.0f                                   //!< 导航条高度
+#define RW_TAB_BAR_HEIGHT ([UIDevice isPhoneX] ? 83.0f : 49.0f)    //!< TabBar高度
+#define RW_TOOL_BAR_HEIGHT ([UIDevice isPhoneX] ? 83.0f : 44.0f)   //!< ToolBar高度
+#define RW_TOP_BAR_HEIGHT ([UIDevice isPhoneX] ? 88.0f : 64.0f)    //!< 状态栏+导航条的高度
+#define RW_SPLIT_LINE_WIDTH (1.0f / RW_SCREEN_SCALE)              //!< 分割线宽度或高度
+#define RW_BOTTOM_SAFEAREA_HEIGHT ([UIDevice isPhoneX] ? 34.0f : 0.0f) //!< iPhone X 底部安全区域高度
+
+#define iPhoneX ([UIDevice isPhoneX])
+
+#pragma mark - Color
+#define RGB(r, g, b) [UIColor colorWithRed:((r) / 255.0) green:((g) / 255.0) blue:((b) / 255.0) alpha:1.0]
+#define RGBAlpha(r, g, b, a) [UIColor colorWithRed:((r) / 255.0) green:((g) / 255.0) blue:((b) / 255.0) alpha:(a)]
+
+// 以十六进制方式传入，例如 HexRGB(0x005DA9)、HexRGB(color_main) 等
+#define HexRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+#define HexRGBAlpha(rgbValue, a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:(a)]
+
+// 以字符串方式传入，例如 HexColor(@"#EEEEEE")、HexColor(kBackColor)
+#define HexColor(hexString) [UIColor colorWithHex:hexString]
+#define HexColorWithAlpha(hexString, a) [UIColor colorWithHex:hexString alpha:a]
+
+#define kMainColor @"#4D7FFF"
+#define kBackGroundColor @"#F3F5F9"
+
+
 #endif /* RWCommonMacro_h */
